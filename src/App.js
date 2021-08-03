4084417aab8801d648eb66c522212bc2f1607c37
jordan-tuinman <jojotuijo@gmail.com>
2021-08-03 18:15:22 +1200

    create basic youtube player component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Components:
 import SearchBar from './components/SearchBar'
 import SearchResults from './components/SearchResults'
+import YTPlayer from './components/YTPlayer'
 
 const App = () => {
   return (
     <>
       <SearchBar />
       <SearchResults />
+      <YTPlayer />
     </>
   )
 }
---
+++ src/components/YTPlayer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const YTPlayer = () => {
+  // let vidId =
+
+  return (
+    <>
+      <iframe
+        title="YTPlayer"
+        // src={`https://www.youtube.com/embed/${vidId}`}
+        width="100%"
+        height="100%"
+      ></iframe>
+    </>
+  )
+}
+
+export default YTPlayer
