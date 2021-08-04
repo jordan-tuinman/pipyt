cd9a7f345862d9f0abb1de8857ecd7c83ccdfc72
jordan-tuinman <jojotuijo@gmail.com>
2021-08-04 17:07:02 +1200

    seperate apikey from get request url

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,11 @@
 import request from 'superagent'
 
+const apiKey = 'AIzaSyBRhFq4yyRuakWGiNNyQyoU5sk7aZ61o7Q'
+
 export function getVideos(query) {
   return request
     .get(
-      `https://youtube.googleapis.com/youtube/v3/search?autoplay=1&type=video&part=snippet&maxResults=25&q=${query}&key=AIzaSyBRhFq4yyRuakWGiNNyQyoU5sk7aZ61o7Q`
+      `https://youtube.googleapis.com/youtube/v3/search?autoplay=1&type=video&part=snippet&maxResults=25&q=${query}&key=${apiKey}`
     )
     .then((res) => {
       return res.body.items
